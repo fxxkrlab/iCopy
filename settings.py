358e1dd228579439b84ef5cca170df4bf8974bb9
Neobarnett <neobarnett@Neobarnetts-MacPro.local>
2020-05-22 23:02:31 +0900

    delete test Token

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TELEGRAM_API_TOKEN", '1092917717:AAGPTG8s-y38so6HNS1ubnA6ubUnHTpqWBU')
+TOKEN = os.environ.get("TELEGRAM_API_TOKEN", '')
 
-ENABLED_USERS = os.environ.get("ENABLED_USERS", '822528623')
+ENABLED_USERS = os.environ.get("ENABLED_USERS", '')
 ENABLED_USERS = set(int(e.strip()) for e in ENABLED_USERS.split(','))
