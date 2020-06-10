8de552912357ddaa1a553415eb736cba32b60f67
Neobarnett <neobarnett@Neobarnetts-MacPro.local>
2020-06-11 03:22:51 +0900

    UPDATE 202006110322 add Specify Customize Target Directory on Copy Mode

--- iCopy.py
+++ iCopy.py
@@ -264,6 +264,8 @@ def main():
 
     dp.add_handler(conv_handler)
 
+    dp.add_handler(CommandHandler("start", start))
+
     dp.add_handler(CommandHandler("help", help))
 
     dp.add_error_handler(error)
