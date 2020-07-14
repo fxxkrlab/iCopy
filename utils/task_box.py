6ca3a569ab3bb328a2665dc7f32cbc07b4a851cb
fxxkrlab <worldfxxkr@gmail.com>
2020-07-14 19:57:27 +0900

    Fix pymongo ERROR too many

--- utils/task_box.py
+++ utils/task_box.py
@@ -7,19 +7,19 @@ from multiprocessing import Process as _mp
 
 myclient = pymongo.MongoClient(f"mongodb+srv://{load.user}:{load.passwd}@{load.cfg['database']['db_addr']}",port=load.cfg['database']['db_port'],connect=False)
 mydb = myclient[load.cfg['database']['db_name']]
-#task_col = mydb['task_col']
+
 task_list = mydb['task_list']
 db_counters = mydb['counters']
-future = task_list.find_one({"_id": "task_list_id"})
+future = db_counters.find_one({"_id": "task_list_id"})
 future_id = 0
 
 if future != None:
     future_id = future['future_id']
 
-
 def cook_task_to_db(update, context, tmp_task_list):
-    global future_id
+
     for item in tmp_task_list:
+        global future_id
         future_id += 1
         item["_id"] = future_id
         item["status"] = 0
@@ -27,6 +27,7 @@ def cook_task_to_db(update, context, tmp_task_list):
         item["create_time"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         item["finished_time"] = ""
 
+    print(tmp_task_list)
     insert_callback = task_list.insert_many(tmp_task_list)
     if insert_callback.inserted_ids:
         db_counters.update({"_id": "task_list_id"},{"future_id":future_id},upsert=True)
