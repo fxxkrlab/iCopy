e5cdcd0664153a94c3676ea5c3b52268d6221c5d
fxxkrlab <worldfxxkr@gmail.com>
2020-08-13 03:30:21 +0900

    Update v0.2.1

--- utils/task_payload.py
+++ utils/task_payload.py
@@ -115,6 +115,8 @@ def task_process(chat_id, command, task, ns):
     task_eta_in_file = "-"
     task_current_prog_size_tail = ""
     task_total_prog_size_tail = ""
+    dst_id = task['dst_id']
+    src_name = task['src_name']
     start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     for toutput in run(command):
